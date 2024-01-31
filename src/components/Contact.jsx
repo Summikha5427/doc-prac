36e2d35403b65bfad629cdbda13e1e48f41bd226
Summikha5427 <summikhan5427@gmail.com>
2024-01-30 21:01:55 -0600

    trying to keep out the merge

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div name='contact' className='w-full md:h-screen bg-[#0a192f] flex justify-center items-center p-4'>
+    <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
         <form method='POST' action="https://getform.io/f/57a89a37-3a8f-40d6-b121-1a45e587ddf3" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-purple-400 text-gray-300'>Contact</p>
