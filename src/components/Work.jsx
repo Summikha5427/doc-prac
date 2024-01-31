03b13a8b645f2f7bc75b2f613276642cab149108
Summikha5427 <summikhan5427@gmail.com>
2024-01-30 20:34:01 -0600

    changes for wok component screen from md to hscreen

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -8,7 +8,7 @@ import enm from "../assets/enm.png";
 
 const Work = () => {
   return (
-    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+    <div name='work' className='w-full h-screen text-gray-300 bg-[#0a192f]'>
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div className='pb-8'>
           <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-purple-400'>
@@ -18,7 +18,7 @@ const Work = () => {
         </div>
 
 {/* container for projects */}
-<div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+<div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4 ">
  {/* CARD-item {projects-1} */}
  <div
             style={{ backgroundImage: `url(${ttt})` }}
